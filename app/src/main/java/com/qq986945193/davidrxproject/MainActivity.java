9c027099fb8104fc7f7612df32be5732efc24602
qq986945193 <986945193@qq.com>
2017-01-13 14:16:13 +0800

    commit

--- app/src/main/java/com/qq986945193/davidrxproject/MainActivity.java
+++ app/src/main/java/com/qq986945193/davidrxproject/MainActivity.java
@@ -1,4 +1,11 @@
 package com.qq986945193.davidrxproject;
+/*
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ * @CSDN博客: http://blog.csdn.net/qq_21376985
+ * @OsChina空间: https://my.oschina.net/mcxiaobing
+ */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
